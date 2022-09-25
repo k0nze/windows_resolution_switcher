1e5eabae66ad5e28e4603384cb9f7b047059df8b
Elijah Lopez <elijahllopezz@gmail.com>
2022-09-25 01:09:10 -0400

    Use multiprocessing to get initial raw resolution

--- resolution_switcher.py
+++ resolution_switcher.py
@@ -1,4 +1,5 @@
 import ctypes
+import multiprocessing as mp
 from contextlib import suppress
 from functools import lru_cache
 
@@ -19,11 +20,26 @@ def get_aspect_ratio(width, height):
     return round(width / height, 2)
 
 
-@lru_cache(maxsize=1)
-def get_initial_res():
+def get_current_res(w=None, h=None):
     user32 = ctypes.windll.user32
     user32.SetProcessDPIAware()
-    return (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+    res = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+    if w is not None:
+        w.value = res[0]
+    if h is not None:
+        h.value = res[1]
+    return res
+
+
+@lru_cache(maxsize=1)
+def get_initial_res():
+    w = mp.Value(ctypes.c_int, 0)
+    h = mp.Value(ctypes.c_int, 0)
+    # use setProcessDPIAware in only child process
+    p = mp.Process(target=get_current_res, args=[w, h])
+    p.start()
+    p.join()
+    return w.value, h.value
 
 
 @lru_cache(maxsize=1)
@@ -33,10 +49,7 @@ def get_initial_dpi_scale():
     return raw_res[0] / transformed_res[0]  # 125% is 1.25
 
 
-# save cache
-get_initial_dpi_scale()
-
-
+@lru_cache(maxsize=2)
 def get_all_resolutions():
     i = 0
     resolutions = []
@@ -57,8 +70,9 @@ def get_all_resolutions():
             i += 1
     aspect_ratio = get_aspect_ratio(max_width, max_height)
     # return resolutions with same aspect ratio as max resolution
-    return sorted(filter(lambda res: get_aspect_ratio(*res) == aspect_ratio, resolutions),
+    lst = sorted(filter(lambda res: get_aspect_ratio(*res) == aspect_ratio, resolutions),
                                 key=lambda item: item[1])
+    return {fmt_res(*res): {'w': res[0], 'h': res[1], 'dpi_scale': calc_dpi_scale(*res)} for res in lst}
 
 
 dpi_vals = [1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 3.00, 3.50, 4.00, 4.50, 5.00]
@@ -72,7 +86,16 @@ def get_recommended_dpi_idx():
     raise IndexError
 
 
-def set_resolution(width: int, height: int):
+def calc_dpi_scale(new_w, _):
+    # assume constant aspect ratios
+    dpi_scale = get_initial_dpi_scale()
+    initial_w = get_initial_res()[0]
+    res_change = 1 - min(new_w, initial_w) / max(new_w, initial_w)
+    dpi_scale += res_change if new_w > initial_w else -res_change
+    return dpi_scale
+
+
+def set_resolution(width: int, height: int, dpi_scale: int):
     # adapted from Peter Wood: https://stackoverflow.com/a/54262365
     devmode = pywintypes.DEVMODEType()
     devmode.PelsWidth = width
@@ -83,10 +106,7 @@ def set_resolution(width: int, height: int):
 
     if CHANGE_DPI_SCALE:
         # https://stackoverflow.com/a/62916586/7732434
-        dpi_scale = get_initial_dpi_scale()
-        initial_w = get_initial_res()[0]
-        res_change = 1 - min(width, initial_w) / max(width, initial_w)
-        dpi_scale += res_change if width > initial_w else -res_change
+        # dpi_scale = calc_dpi_scale(width, height)
         with suppress(KeyError, IndexError):
             ref_idx = get_recommended_dpi_idx()
             # dpi of 1.5 -> 2 - 1 = rel index of 1
@@ -95,14 +115,14 @@ def set_resolution(width: int, height: int):
             ctypes.windll.user32.SystemParametersInfoA(0x009F, rel_idx, 0, 1)
 
 
-def set_res_curry(width, height):
+def set_res_curry(width, height, dpi_scale):
     # ensure correct values are used when lambda executes
-    return lambda: set_resolution(width, height)
+    return lambda: set_resolution(width, height, dpi_scale)
 
 
-def get_res_item_text(width, height, show_height=False):
+def fmt_res(width, height, show_width=False):
     # formats either W x H or Wp
-    return f'{width} x {height}' if show_height else f'{width}p'
+    return f'{width} x {height}' if show_width else f'{height}p'
 
 
 def on_exit():
@@ -111,9 +131,11 @@ def on_exit():
 
 
 if __name__ == '__main__':
-
+    mp.freeze_support()
+    # save cache
+    get_initial_dpi_scale()
     image = Image.open('icon.png')
-    menu = [item(f'{res[1]}p', set_res_curry(*res)) for res in get_all_resolutions()]
+    menu = [item(k, set_res_curry(v['w'], v['h'], v['dpi_scale'])) for k, v in get_all_resolutions().items()]
     menu.append(item('Exit', on_exit))
     icon = pystray.Icon('Resolution Switcher', image, 'Resolution Switcher', menu)
     icon.run()
