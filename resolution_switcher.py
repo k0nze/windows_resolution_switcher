9e7eeee703a27af4acb288e4128be10b2f80099b
Elijah Lopez <elijahllopezz@gmail.com>
2022-09-24 20:59:22 -0400

    Get list of resolutions dynamically

--- resolution_switcher.py
+++ resolution_switcher.py
@@ -1,9 +1,39 @@
+from contextlib import suppress
 import pystray
 import pywintypes
 import win32api
 import win32con
 from PIL import Image
 from pystray import MenuItem as item
+
+
+def get_aspect_ratio(width, height):
+    return round(width / height, 2)
+
+
+def get_all_resolutions():
+    i = 0
+    resolutions = []
+    seen = set()
+    max_width = 0
+    max_height = 0
+    with suppress(Exception):
+        while True:
+            ds = win32api.EnumDisplaySettings(None, i)
+            res = (ds.PelsWidth, ds.PelsHeight)
+            if res not in seen:
+                seen.add(res)
+                if ds.PelsWidth > max_width:
+                    max_width = ds.PelsWidth
+                if ds.PelsHeight > max_height:
+                    max_height = ds.PelsHeight
+                resolutions.append((ds.PelsWidth, ds.PelsHeight))
+            i += 1
+    aspect_ratio = get_aspect_ratio(max_width, max_height)
+    # return resolutions with same aspect ratio as max resolution
+    return sorted(filter(lambda res: get_aspect_ratio(*res) == aspect_ratio,
+                         resolutions),
+                  key=lambda item: item[1])
 
 
 def on_set_resolution(width: int, height: int):
@@ -17,20 +47,27 @@ def on_set_resolution(width: int, height: int):
     win32api.ChangeDisplaySettings(devmode, 0)
 
 
-def on_quit():
+def set_res_curry(width, height):
+    # ensure correct values are used when lambda executes
+    return lambda: on_set_resolution(width, height)
+
+
+def get_res_item_text(width, height, show_height=False):
+    # formats either W x H or Wp
+    return f'{width} x {height}' if show_height else f'{width}p'
+
+
+def on_exit():
     icon.visible = False
     icon.stop()
 
+if __name__ == '__main__':
+    image = Image.open('icon.png')
+    menu = [
+        item(f'{res[1]}p', set_res_curry(*res))
+        for res in get_all_resolutions()
+    ]
+    menu.append(item('Exit', on_exit))
 
-if __name__ == "__main__":
-    # adapted from Sebastin Ignacio Camilla Trinc: https://stackoverflow.com/a/48284187
-    image = Image.open("icon.png")
-
-    menu = (
-        item('2160p', lambda: on_set_resolution(3840, 2160)), 
-        item('1440p', lambda: on_set_resolution(2560, 1440)),
-        item('Quit', on_quit)
-        )
-
-    icon = pystray.Icon("Resolution Switcher", image, "Resolution Switcher", menu)
+    icon = pystray.Icon('Resolution Switcher', image, 'Resolution Switcher', menu)
     icon.run()
